829df1906fe14f392c22c629d8e026478a86ced5
Alex He <alexhe2001@gmail.com>
2021-09-18 20:23:03 -0400

    Still fixing geocoding on API

--- pages/api/new-listing.js
+++ pages/api/new-listing.js
@@ -4,31 +4,32 @@ import NodeGeocoder from 'node-geocoder';
 async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-
-    const geocoder = NodeGeocoder({
-      provider: 'google',
-      apiKey: process.env.GOOGLE_MAP_KEY,
-    });
-    const results = await geocoder.geocode(data.location)
-    data.coordinates[0] = results[0].latitude;
-    data.coordinates[1] = results[0].longitude;
-
     const client = await MongoClient.connect(process.env.DB_CONNECTION);
     const db = client.db();
-
     const listingsCollection = db.collection("listings");
 
     // error handle with try catch?
     if (data.image == "") {
-      res.status(400).json({ message: "Image not provided!" });
       client.close();
+      res.status(400).json({ message: "Image not provided!" });
+    } else if (data.location == "") {
+      client.close();
+      res.status(400).json({ message: "Location not provided!" });
+    } else {
+      const geocoder = NodeGeocoder({
+        provider: 'google',
+        apiKey: process.env.GOOGLE_MAP_KEY,
+      });
+      const results = await geocoder.geocode(data.location)
+      data.coordinates[0] = results[0].latitude;
+      data.coordinates[1] = results[0].longitude;
+
+      const result = await listingsCollection.insertOne(data);
+
+      client.close();
+
+      res.status(201).json({ message: "Listing inserted!" });
     }
-
-    const result = await listingsCollection.insertOne(data);
-
-    client.close();
-
-    res.status(201).json({ message: "Listing inserted!" });
   }
 }
 
